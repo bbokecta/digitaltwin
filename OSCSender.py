db6da60ed3a41a600ef3b384121a0a241f006b20
Bami <bami.oke@hogarth.com>
2025-03-25 23:17:10 +0000

    updated data sent thru post

--- OSCSender.py
+++ OSCSender.py
@@ -1,35 +1,13 @@
 from pythonosc.udp_client import SimpleUDPClient
 import time
 
-ip = "10.106.32.214"
+ip = "192.168.0.51"
 port = 8000
+# global last_pose
+
 
 def main(values):
-    
-
-    pose_names = list(values.keys())
-    pose_values = list(values.values())
-    pose_results = []
-    
-    for pose in pose_names:
-        if float(values[pose]) > 0.75:
-            # print(pose)
-            send_message(str(pose))
-        
-        # print(pose_results[:4])
-        
-        # if float(values[pose]) > 0.75:
-    
-        #     # send_message(str(pose))
-        #     print(str(pose))
-        #     time.sleep(8)
-        # else:
-        #     print("waiting")
-        # else:
-        #     print("none")
-
-    # for pose in pose_values:
-    #     print(pose_values[pose].keys)
+    send_message(values['poses'])
 
   
 
--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from OSCSender import main
 
 app = Flask(__name__, static_url_path='/static')
 
+
 @app.route('/')
 def index():
 
@@ -11,11 +12,11 @@ def index():
 @app.route('/post', methods=['POST'])
 def process():
     data = request.get_json()
+    # print(data)
     main(data)
-
     return data
 
-@app.route('/home.html')
-def home():
+# @app.route('/home.html')
+# def home():
 
-    return render_template('home.html')
+#     return render_template('home.html')
